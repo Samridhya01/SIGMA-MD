04841e50d16d033260f4686c4ada7eaa0c2d5f21
Samridhya Santra <125868472+Samridhya01@users.noreply.github.com>
2024-04-26 16:53:35 +0530

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -75,7 +75,7 @@ global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/03e49e6
 module.exports = {
 
   //==============================[COMPULSORY]=====================================
-  sessionName: process.env.SESSION_ID || '',              // PUT YOUR SESSION ID
+  sessionName: process.env.SESSION_ID || '26_04_2024_SIGMA_MD_iK9P7Mda_MAHER_ZUBAIR',              // PUT YOUR SESSION ID
   botname: process.env.BOT_NAME       || 'Samrat CS',         // ADD YOUR BOT NAME
   ownername:  process.env.OWNER_NAME  || `Samriddho`, // ADD YOUR NAME
   HANDLERS: process.env.PREFIX        || '.',              // ADD YOUR BOT PREFIX
